17359ed1b0f133409dc854472cab94feff97b39c
aa57c <aa57c@umsystem.edu>
2024-07-26 09:33:32 -0500

    edited github data object and projects to include deep learning experiments project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,6 +10,9 @@ const Projects = () => {
                 <i className="fa-regular fa-folder-open folder-icon"></i>
                 <div className="small-icons">
                   <a href={project.githublink} aria-label="Github" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
+                  {project.downloadfilelink === true && project.title === "Deep Learning Experiments" && (
+                    <a href="/files/Experiment_Results.zip" aria-label="Zip File" target="_blank" rel="noopener noreferrer"><i className="fa-solid fa-download"></i></a>
+                  )}
                 </div>
               </div>
               <h3>{project.title}</h3>
--- src/components/github_data.js
+++ src/components/github_data.js
@@ -3,13 +3,23 @@ export const projectData = [
         id: 1,
         title: 'Cryptography Web App',
         description: 'A simple solution to patient data storage using a combination of cryptography, including AES (Advanced Encryption Standard), ECC (Elliptical Curve Cryptography), and PQC (Post-Quantum Cryptography). The app is run on Docker and the data is stored in a PostGre SQL Database. The server for the database also runs on Docker. This was a group project during graduate school.',
-        githublink: "https://github.com/aa57c/CryptographyProject"
+        githublink: "https://github.com/aa57c/CryptographyProject",
+        downloadfilelink: false
     },
     {
         id: 2,
         title: 'Nine Men\'s Morris Game',
         description: 'A Python implementation of a Nine Men\'s Morris board game. Tkinter library was used for the graphical user interface. This was a group project during graduate school.',
-        githublink: "https://github.com/aa57c/NineMensMorrisPython"
+        githublink: "https://github.com/aa57c/NineMensMorrisPython",
+        downloadfilelink: false
+    },
+    {
+        id: 3,
+        title: 'Deep Learning Experiments',
+        description: 'Four experiments that are conducted using currently trained as well as custom made classifcation AI models (Segformer, VGG, Resnet, etc). I recommend running the Jupyter Notebooks on Google Colab with a GPU to have results faster. Click the download link to see my results and best models I have configured.',
+        githublink: "https://github.com/aa57c/Deep_Learning_Experiments",
+        downloadfilelink: true
+
     }
     
 ]
