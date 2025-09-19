6e1882396ef7b307923fcb5e7498ac93415bb043
aa57c <aa57c@umsystem.edu>
2025-09-18 23:12:14 -0500

    undid coming soon project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -75,13 +75,6 @@ const Projects = () => {
         
         
         ))}
-        <div className="project-card coming-soon">
-          <div className="project-header">
-            <i className="fa-regular fa-hourglass-half folder-icon"></i>
-          </div>
-          <h3>Coming Soon... AI Powered News Bias Detector</h3>
-          <p>Stay tuned for my next big project!</p>
-        </div>
       </div>
     </div>
   );
