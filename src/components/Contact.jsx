bec20e4934b29bd3133f0337f34922d54e31b77b
aa57c <aa57c@umsystem.edu>
2025-09-27 14:27:59 -0500

    edited contact info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,9 @@
 const Contact = () => {
     return (
-       <div className="contact-container">
+      <div className="contact-container">
         <h2>Get In Touch</h2>
         <p>If you want us to work together, have any questions or want me to speak at your event, my inbox is always open. Whether I just want to say hi, I'll try my best to get back to you! Cheers!</p>
-        <div className="contact-options">
-          <a 
-            href="https://mail.google.com/mail/?view=cm&to=ashna.ali.prof@gmail.com" 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            className='cta-btn'
-          >
-            Email via Gmail
-          </a>
-          <a href="mailto:ashna.ali.prof@gmail.com" className='cta-btn'>
-            Email via Default Client
-          </a>
-        </div>
+        <a href="mailto:ashna.ali.prof@gmail.com" className='cta-btn'>Say Hello</a>
       </div>
     )
 }
